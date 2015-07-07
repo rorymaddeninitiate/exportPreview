a7e886592a8eb12cef34e835b24e90a22dd59643
Rory Madden <rorymadden@gmail.com>
2015-07-07 22:12:09 +0100

    Proper CNAME build

--- app/scripts/directives/adminMenu/adminMenuDirective.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('adminMenuDirective', [])
-.directive('adminMenu', function() {
-  return {
-    restrict: 'E',
-    templateUrl: 'scripts/directives/adminMenu/adminMenu.html'
-  };
-});
